78717fe69d0f1d938c4104bd1341813b98040642
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-08-19 21:25:42 +0100

    Create index.ts

---
+++ src/components/common/index.ts
@@ -0,0 +1,6 @@
+// src/components/common/index.ts - Common components export
+export { default as Button } from './Button/Button';
+export { default as Card } from './Card/Card';
+export { default as Badge } from './Badge/Badge';
+export { default as Table } from './Table/Table';
+export { default as Modal } from './Modal/Modal';
