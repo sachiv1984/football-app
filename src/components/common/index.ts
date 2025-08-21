44c02996a6aca8397d6d8dee210318cbcb8525ac
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-08-21 09:18:56 +0100

    Delete src/components/common/index.ts

--- src/components/common/index.ts
+++
@@ -1,6 +0,0 @@
-// src/components/common/index.ts - Common components export
-export { default as Button } from './Button/Button';
-export { default as Card } from './Card/Card';
-export { default as Badge } from './Badge/Badge';
-export { default as Table } from './Table/Table';
-export { default as Modal } from './Modal/Modal';
